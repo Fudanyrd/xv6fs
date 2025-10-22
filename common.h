7f8d732b6a1d29e1e5e0c3e279c2f6c24d63c67a
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2025-10-22 11:43:38 +0800

    [IMPL] assert, warning and error

--- common.h
+++ common.h
@@ -3,11 +3,24 @@
 
 typedef unsigned int uint;
 typedef unsigned short ushort;
+typedef unsigned char uchar;
 
-#define xv6_assert(cond) do {} while (0)
+#define xv6_assert(cond) do {                                         \
+    if (! (cond)) {                                                   \
+        panic("xv6: internal error at %s:%d\n", __FILE__, __LINE__);  \
+    }                                                                 \
+} while (0)
 
 #define xv6_static_assert(cond) \
     do { switch(cond) { case (0): break; case (cond): break; }; } while (0)
+
+#define xv6_warn(fmt, ...) do {                           \
+    printk(KERN_WARNING "xv6: " fmt "\n", ##__VA_ARGS__); \
+} while (0)
+
+#define xv6_error(fmt, ...) do {                          \
+    printk(KERN_ERR "xv6: " fmt "\n", ##__VA_ARGS__);     \
+} while (0)
 
 #endif // _COMMON_H 1
 
